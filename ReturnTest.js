2ce2681f3a98c65f977f595a993cae2bdf62ddc2
TakoyCode <129565940+TakoyCode@users.noreply.github.com>
2024-03-04 14:21:58 +0100

    moved randomizeLampIndex

--- ReturnTest.js
+++ ReturnTest.js
@@ -10,14 +10,12 @@ let finishTime = 0;
 let spentSeconds = 0;
 
 //View
-randomizeLampIndex();
-
 function updateView() {
     let html = /*HTML*/ `
     
     <div class="center">
-        <div id="LampGrid">${createCirclesHtml()}</div>    
-        <div class="timertext">Dette er din rekasjons tid: <br> ${spentSeconds} sek</div>    
+    <div id="LampGrid">${createCirclesHtml()}</div>    
+    <div class="timertext">Dette er din rekasjons tid: <br> ${spentSeconds} sek</div>    
     <div>
     `;
     app.innerHTML = html;
@@ -33,9 +31,9 @@ function createCirclesHtml() {
     let html = ``;
     for (let counter = 1; counter <= 25; counter++) {
         html += /*HTML*/ `
-
+        
         <div id="lamp${counter}" class="lamp"></div>
-
+        
         `;
     }
     return html;
@@ -43,6 +41,8 @@ function createCirclesHtml() {
 
 
 //Controller
+randomizeLampIndex();
+
 function reactionTimeTimer() {
     if (firstClick == true) {
         startTime = new Date().getTime();
@@ -71,7 +71,7 @@ function randomizeLampIndex() {
     let min = Math.ceil(1);
     let max = Math.floor(26);
     let lastSelectedLamp = selectedLampIndex;
-
+    /
     while (lastSelectedLamp == selectedLampIndex) {
         selectedLampIndex = Math.floor(Math.random() * (max - min) + min);
     }
